288c2b044e7aa70248062f77b3ed3ff5a6a4dce7
Lipeeeh14 <lipeeeh14@gmail.com>
2021-06-06 14:55:53 -0300

    Seção 10

---
+++ 06_RestWithASPNETUdemy_SplittingLogic/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness // service
+    {
+
+        private readonly IPersonRepository _repository;
+
+        public PersonBusinessImplementation(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // Método responsável por retornar todas as pessoas do DB
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        // Buscar uma pessoa por ID
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        // Cria uma nova pessoa
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        // atualiza uma pessoa
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        // deleta a pessoa por id
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ 06_RestWithASPNETUdemy_SplittingLogic/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASPNETUdemy.Repository.Implementations
+{
+    public class PersonRepositoryImplementation : IPersonRepository
+    {
+
+        private MySQLContext _context;
+
+        public PersonRepositoryImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        // Método responsável por retornar todas as pessoas do DB
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        // Buscar uma pessoa por ID
+        public Person FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        // Cria uma nova pessoa
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
+        }
+
+        // atualiza uma pessoa
+        public Person Update(Person person)
+        {
+            // Verificamos se a pessoa existe no banco de dados
+            // Se não existir, retornamos uma instância de pessoa vazia
+            if (!Exists(person.Id)) return new Person();
+
+            // Recebe a pessoa do DB
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    // set as alterações e salva
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return person;
+        }
+
+        // deleta a pessoa por id
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        // verifica se a pessoa existe no DB
+        public bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}
