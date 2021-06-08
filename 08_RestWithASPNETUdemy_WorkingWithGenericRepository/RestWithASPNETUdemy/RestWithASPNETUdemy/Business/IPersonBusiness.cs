e61d9b5c67a601dfcad4b94abc026b64e59f8e92
Lipeeeh14 <lipeeeh14@gmail.com>
2021-06-08 13:39:07 -0300

    Seção 12

---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IPersonBusiness // service
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,47 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness // service
+    {
+
+        private readonly IRepository<Person> _repository;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+        }
+
+        // Método responsável por retornar todas as pessoas do DB
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        // Buscar uma pessoa por ID
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        // Cria uma nova pessoa
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        // atualiza uma pessoa
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        // deleta a pessoa por id
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
