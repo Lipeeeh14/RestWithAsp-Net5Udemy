f457bfe774619c967bce0c9e4331a03f00219775
Lipeeeh14 <lipeeeh14@gmail.com>
2021-06-10 12:23:02 -0300

    Seção 20

---
+++ 16_RestWithASPNETUdemy_PATCHVerb/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Filters;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")] // versão da api
+    [ApiController]
+    [Authorize("Bearer")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        // Declarando o serviço utilizado
+        private IPersonBusiness _personBusiness;
+
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        // Pesquisa todos os registros
+        [HttpGet]
+        [ProducesResponseType((200), Type = typeof(List<PersonVO>))] // caso de sucesso
+        [ProducesResponseType((204))]
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        // Pesquisa por id
+        [HttpGet("{id}")]
+        [ProducesResponseType((200), Type = typeof(PersonVO))] // caso de sucesso
+        [ProducesResponseType((204))]
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        // insere um novo registro
+        [HttpPost]
+        [ProducesResponseType((200), Type = typeof(PersonVO))] // caso de sucesso
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Create(person));
+        }
+
+        // Atualiza um registro já existente
+        [HttpPut]
+        [ProducesResponseType((200), Type = typeof(PersonVO))] // caso de sucesso
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Update(person));
+        }
+
+        [HttpPatch("{id}")]
+        [ProducesResponseType((200), Type = typeof(PersonVO))] // caso de sucesso
+        [ProducesResponseType((204))]
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Patch(long id)
+        {
+            var person = _personBusiness.Disable(id);
+            return Ok(person);
+        }
+
+        // deleta um registro a partir do id
+        [HttpDelete("{id}")]
+        [ProducesResponseType((204))] // caso de sucesso
+        [ProducesResponseType((400))]
+        [ProducesResponseType((401))]
+        public IActionResult Delete(long id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
